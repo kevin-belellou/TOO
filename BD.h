35b5a040e650080f43ed969b082b7c756d05d6c9
Xarcan <charlycarrere@gmail.com>
2011-12-17 17:53:52 +0100

    Doxygen

--- BD.h
+++ BD.h
@@ -3,19 +3,41 @@
 
 #include "Livre.h"
 
+/**
+ * Classe BD qui hérite de Livre 
+ */
 class BD : public Livre
 {
     public:
-        // Constructeur
+
+        /**
+         * Constructeur surchargé de BD
+         * @param titre string
+         * @param auteur string
+         * @param editeur string
+         * @param dessinateur
+         */
         BD(string titre, string auteur, string editeur, string dessinateur);
 
-        // Destructeur
+        /**
+         * Destructeur de BD
+         */
         virtual ~BD();
 
-        // M�thodes
+        /**
+         * Getteur de Dessinateur
+         */
         string getDessinateur() const;
+        
+        /**
+         * Setteur de Dessinateur
+         * @param val string
+         */
         void setDessinateur(string val);
 
+        /**
+         * méthode permettant d'afficher l'objet BD
+         */
         virtual string afficher() const;
     protected:
     private:
