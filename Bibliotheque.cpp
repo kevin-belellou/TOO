b9fafe6d4e80a21060225617bdf15acbcf50a9c2
Charly Carrere <charlycarrere@gmail.com>
2012-01-10 15:56:52 +0100

    Commentaires sur les méthodes

--- Bibliotheque.cpp
+++ Bibliotheque.cpp
@@ -1,6 +1,6 @@
 #include "Bibliotheque.h"
 
-// Constructeur
+// Constructeur Par défaut
 Bibliotheque::Bibliotheque()
 {
 
@@ -14,6 +14,10 @@ Bibliotheque::~Bibliotheque()
 
 // Méthodes
 
+/*
+Cette méthode permet d'ajouter un document dansla bibliothèque.
+On ajoute dans le vecteur ainsi que dans le fichier "Bibli.txt" qui sert de Base de données. Le fait d'ajouter dans un vecteur facilite le trie, la recherche, la supression etc... De plus cela évite d'ouvrir et de fermer le fichier "Bibli.txt", ce qui permet une meilleur stabilité.
+*/
 void Bibliotheque::ajouter(Document* doc)
 {
 
@@ -27,9 +31,13 @@ void Bibliotheque::ajouter(Document* doc)
 		}
 		
 		this->_bibli.push_back(doc);
-		doc->ajout();	
+		doc->ajout();	// On fait appel à la fonction ajout en utilisant le polymorphisme, permettant d'ajouter dans la BDD
 }
 
+
+/*
+Cette méthode recherche et supprime le document envoyé en paramètre. Elle le supprime dans le vecteur, et ensuite on efface tout le contenu de la BDD et on recopie chaque élément du vecteur dans le fichier.
+*/
 void Bibliotheque::supprimer(Document* doc)
 {
     for (unsigned int i(0); i < this->_bibli.size(); i++)
@@ -46,7 +54,7 @@ void Bibliotheque::supprimer(Document* doc)
     }
     
 
-
+	//On Ouvre le fichier en effaçant le contenu, et on le ferme.
     ofstream fichier("Bibli.txt", ios::out | ios::trunc);
    	if(fichier)  // si l'ouverture a réussi
    	{     
@@ -55,6 +63,7 @@ void Bibliotheque::supprimer(Document* doc)
     else
      cerr << "Erreur à l'ouverture !" << endl;
 	
+	//On ajoute pour chaque élément du vecteur, le contenu dans la BDD
 	for (unsigned int u(0); u < this->_bibli.size(); u++)
 	{
        this->_bibli[u]->ajout();
@@ -63,6 +72,9 @@ void Bibliotheque::supprimer(Document* doc)
 }
 
 /*
+
+//Ceci est l'affichage lors de la première version, quand on avait pas implémenté de BDD.
+
 void Bibliotheque::afficher() const
 {
     if (this->_bibli.size() != 0)
@@ -70,29 +82,40 @@ void Bibliotheque::afficher() const
             cout << i << " : " << _bibli[i]->afficher() << endl;
 }
 */
+
+/*
+On affiche tout le contenu de la BDD
+*/
 void Bibliotheque::afficher() const
 {
+	//On ouvre le fichier en lecture
 	ifstream fichier("Bibli.txt", ios::in);
     if(fichier)  // si l'ouverture a réussi
     {
 		string ligne;
 		while(getline(fichier, ligne))
-        {
+        {		
+        		//On stock chaque ligne du fichier dans un string, que l'on affiche. Et ce jusqu'à la fin du fichier
                 cout << ligne << endl;
         }     
      	fichier.close();  // on referme le fichier
     }
-    else
+    else //On affiche si il y a eu une erreur d'ouverture.
      cerr << "Erreur à l'ouverture !" << endl;
 }
 
 
+/*
+Cette méthode recherhe dans le vecteur si le document existe.
+Nul besoin de chercher dans le fichier, puisqu'il est à jour par rapport au vecteur.
+*/
 void Bibliotheque::recherche(string pSearch) const
 {
 	bool trouve=false;
 	unsigned int i=0;
-	while ( (!trouve) && (i<=_bibli.size()) )
+	while ( (!trouve) && (i<=_bibli.size()) ) //Oncherche tant que le fichier n'est pas trouvé, ou qu'on ai parcouru tout le vecteur.
 	{
+		//Si on trouve on arrête de chercher et on affiche.
 		if(_bibli[i]->getTitre()==pSearch)
 		{
 			trouve=true;
@@ -101,7 +124,7 @@ void Bibliotheque::recherche(string pSearch) const
 		}
 		else i++;
 	}
-
+	//Si on ne trouve pas.
 	if(!trouve)
 	{
 		cout<<"Votre document n'est pas dans la Bibliotheque"<<endl;
@@ -109,7 +132,9 @@ void Bibliotheque::recherche(string pSearch) const
 
 }
 
-
+/*
+Cette méthode tri le vecteur en fonction du type du tri (par Titre ou Auteur).
+*/
 void Bibliotheque::tri(string pTri)
 {
 	bool tab_en_ordre = false;
